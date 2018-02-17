adcd01090ceedecb9227e4e012cac2115f035db2
Carlo <carlo.namoca@gmail.com>
2018-02-16 23:44:30 -0800

    Add storyboards and VCs

---
+++ WavyCheckin/EventsListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventsAndListsViewController.swift
+//  WavyCheckin
+//
+//  Created by Carlo Namoca on 2018-02-16.
+//  Copyright © 2018 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class EventsAndListsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
